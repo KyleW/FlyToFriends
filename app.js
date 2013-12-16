1e534c10efdc4d5f5a24c132e30d5717f7723512
KyleW <Kyle.Warneck@gmail.com>
2013-12-16 11:53:09 -0800

    parse deployment now working

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ window.fbAsyncInit = function() {
   FB.Event.subscribe('auth.authResponseChange', function(response) {
     // Here we specify what we do with the response anytime this event occurs. 
     if (response.status === 'connected') {
+      console.log(response);
       // The response object is returned with a status field that lets the app know the current
       // login status of the person. In this case, we're handling the situation where they 
       // have logged in to the app.
