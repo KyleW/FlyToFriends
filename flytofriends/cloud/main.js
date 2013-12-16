943916eee0b2774689b0e2003093c7574b6a7137
KyleW <Kyle.Warneck@gmail.com>
2013-12-16 13:45:06 -0800

    have the basic fb login code working and being served correctly by parse

--- flytofriends/cloud/main.js
+++ flytofriends/cloud/main.js
@@ -4,3 +4,4 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
--- flytofriends/public/fb.js
+++ flytofriends/public/fb.js
@@ -12,7 +12,7 @@ window.fbAsyncInit = function() {
   // will be handled. 
   FB.Event.subscribe('auth.authResponseChange', function(response) {
     // Here we specify what we do with the response anytime this event occurs. 
-    if (response.status === 'connected') {
+     if (response.status === 'connected') {
       console.log(response);
       // The response object is returned with a status field that lets the app know the current
       // login status of the person. In this case, we're handling the situation where they 
