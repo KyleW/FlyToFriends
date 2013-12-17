db595e6bd25fea08ad6823e16c9d35363d46adbb
KyleW <Kyle.Warneck@gmail.com>
2013-12-17 12:07:29 -0800

    now checks fb login status and prompts to login on getting started page.

--- flytofriends/public/controllers.js
+++ flytofriends/public/controllers.js
@@ -1,29 +1,76 @@
 angular.module('myControllers',[])
-  .controller('showFlights', function($scope, $http, $location, sharedProperties) {
-    $scope.friendLoc = friendLoc;
-    $scope.setDest= function(newDest){
-      sharedProperties.setDest(newDest);
-      $location.path("/cityDetails");
-    };
-  })
-  .controller('start', function($scope, $http, sharedProperties) {
+.controller('showFlights', function($scope, $http, $location, sharedProperties) {
+  $scope.friendLoc = friendLoc;
+  $scope.setDest= function(newDest){
+    sharedProperties.setDest(newDest);
+    $location.path("/cityDetails");
+  };
+})
+.controller('start', function($scope, $http, $location, sharedProperties) {
+  $scope.checkLogin = function(){
+    FB.getLoginStatus(function(response) {
+      if (response.status === 'connected') {
+        FB.api(
+        {
+          method: 'fql.query',
+          query: 'SELECT current_location FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())'
+        }, function(data) {
+          console.log(data);
+          for (var key in data){
+            if (data[key].current_location){
+              var cur = data[key].current_location.city;
+              if (cur in friendLoc){
+                friendLoc[cur]++;
+              } else {
+                friendLoc[cur] = 1;
+              }
+            }
+          }
+        });
 
-  })
-  .controller('showCity', function($scope, $http, sharedProperties) {
-    $scope.destination = sharedProperties.getDest();
-    $scope.origin = sharedProperties.getOrigin();
+      } else {
+        FB.login(function(response) {}, {scope:'friends_location'});
 
-    var hotwireUrl = "http://api.hotwire.com/v1/tripstarter/air?apikey=j6vujgj99vxdghsjkfzccuuu&origin="+$scope.origin+"&limit=1&DestinationCity="+$scope.destination+"&format=jsonp&callback=JSON_CALLBACK";
-    
-    $http.jsonp(hotwireUrl)
-    .success(function(data){
-      console.log(data.Result.AirPricing);
-      $scope.destinationData = data.Result.AirPricing;
-    }).error(function(){
-      console.log("there was an error in your hotwire request");
+        FB.api(
+        {
+          method: 'fql.query',
+          query: 'SELECT current_location FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())'
+        }, function(data) {
+          console.log(data);
+          for (var key in data){
+            if (data[key].current_location){
+              var cur = data[key].current_location.city;
+              if (cur in friendLoc){
+                friendLoc[cur]++;
+              } else {
+                friendLoc[cur] = 1;
+              }
+            }
+          }
+        });
+      }
+
+      $location.path("/cityList");
+
     });
-    
+  };
+
+})
+.controller('showCity', function($scope, $http, sharedProperties) {
+  $scope.destination = sharedProperties.getDest();
+  $scope.origin = sharedProperties.getOrigin();
+
+  var hotwireUrl = "http://api.hotwire.com/v1/tripstarter/air?apikey=j6vujgj99vxdghsjkfzccuuu&origin="+$scope.origin+"&limit=1&DestinationCity="+$scope.destination+"&format=jsonp&callback=JSON_CALLBACK";
+
+  $http.jsonp(hotwireUrl)
+  .success(function(data){
+    console.log(data.Result.AirPricing);
+    $scope.destinationData = data.Result.AirPricing;
+  }).error(function(){
+    console.log("there was an error in your hotwire request");
   });
+
+});
 
 
 
